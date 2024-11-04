d54a3e85c823111b93f37fed1b8a83dbc1b617d6
LambrosTz <115354473+LambrosTz@users.noreply.github.com>
2024-11-04 23:17:08 +0200

    Add files via upload

--- FEM.py
+++ FEM.py
@@ -11,6 +11,7 @@ class FEM_1D:
     
     def __init__(self, domain, nex, interp='linear'):
         
+        self.interp=interp
         self.nn_el=2
         self.domain=domain
         self.nex=nex
@@ -22,6 +23,9 @@ class FEM_1D:
         self.dirbc=[]
         self.neubc=[]
         
+    def eq_param(self, parameters):
+        
+        self.par=parameters          # u0 , u , du/dx , d2u/dx2
     
     def nop(self, ln, el):
         
@@ -40,58 +44,68 @@ class FEM_1D:
         if len(self.dirbc)>0:
             plt.scatter(self.nodes[self.dirbc[:][0]][0], np.zeros(len(self.dirbc)), color='red')
         plt.show()
+    
+    def ph(self, x):
         
-    def phix(self, j, x):
+        return np.array([1.-x , x])
+    
+    def phd(self):
         
-        if (x>=self.domain[0]) and (x<=self.domain[1]):
-            
-            if (j>0) and (j<(self.nnx-1)):
-            
-                if (x>self.nodes[j-1]) and (x<self.nodes[j+1]):
-                    
-                    if x == self.nodes[j]:
-                        
-                        return 1.
-                    
-                    elif x < self.nodes[j]:
-                        
-                        return (x-self.nodes[j-1])/(self.nodes[j]-self.nodes[j-1])
-                    
-                    elif x > self.nodes[j]:
-                        
-                        return (x-self.nodes[j+1])/(self.nodes[j]-self.nodes[j+1])
-                
-                else:
-                    return 0.
-            
-            elif j==0:
-                
-                if x==self.nodes[0]:
-                    
-                    return 1.
-                
-                else:
-                    
-                    return (x-self.nodes[j+1])/(self.nodes[j]-self.nodes[j+1])
-            
-            elif j==(self.nnx-1):
-                
-                if x==self.nodes[-1]:
-                    
-                    return 1.
-                
-                else:
-                
-                    return (x-self.nodes[j-1])/(self.nodes[j]-self.nodes[j-1])
-                
-        else:
-            
-            print("x out of domain\n")
-            
-            return 0.
+        return np.array([-1., 1.])
+    
+    def abfind(self, nel):
         
-    def plot_phx(self):
-        pass
+        wgp=1.
+        gp=0.5
+        
+        x=0.
+        x1=0.
+        
+        ph = self.ph(gp)
+        phd = self.phd()
+        
+        for n in range(self.nn_el):
+            
+            x=x+self.nodes[self.nop(n,nel)]*ph[n]
+            x1=x1+self.nodes[self.nop(n,nel)]*phd[n] # dx / dξ
+        
+        phx = phd/x1
+        
+        a = self.par[1]
+        #a2 = self.par[3]
+        
+        for m in range(self.nn_el):
+            
+            m1=self.nop(m, nel)
+            
+            for n in range(self.nn_el):
+                
+                n1=self.nop(n, nel)
+                                
+                self.A[m1,n1]=(self.A[m1,n1]
+                               -wgp*x1*phx[m]*phx[n]
+                               +a*wgp*x1*ph[m]*ph[n])
+            
+    
+    def axb(self):
+        
+        self.A=np.zeros((self.nnx,self.nnx))
+        self.b=np.zeros(self.nnx)
+        
+        for nel in range(len(self.elements)):
+            
+            self.abfind(nel)
+        
+        # BC
+        
+        for node, value in self.dirbc:
+            
+            self.A[node]=0.
+            
+            self.A[node, node]=1.
+            
+            self.b[node]=value
+        
         
     
 class FEM_2D:
@@ -140,20 +154,16 @@ class FEM_2D:
         if len(self.dirbc)>0:
             plt.scatter(self.nodes[np.array(self.dirbc)[:,0],0], self.nodes[np.array(self.dirbc)[:,0],1], color='red')
         plt.show()
-
-class ode:
-    pass
-
-    
+  
 
         
 test=FEM_1D((0,10), 6)
 
 test.plotmesh()
 
-test2=FEM_2D(((0,10),(0,5)), (11,6))
+# test2=FEM_2D(((0,10),(0,5)), (11,6))
 
-test2.plotmesh()
+# test2.plotmesh()
 
 
 # test.dir_node(0, 0)
@@ -165,3 +175,10 @@ test2.plotmesh()
 
 # test2.plotmesh()
 
+a=-1.0
+parameters = [0, a, 0, 1]  # u0 , u , du/dx , d2u/dx2
+
+test.dir_node(0, 1)
+
+test.eq_param(parameters)
+test.axb()
